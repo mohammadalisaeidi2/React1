8b72977713d2172e5973756a1b0781b11f57793a
Mohammadali <mohammadalisaeidi@yahoo.com>
2022-11-05 12:43:21 +0330

    files added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
 
 function App() {
+  const [unis , setUnis] = useState([
+    {name:'sharif' , id:1},
+    {name:'tehran' ,id:2},
+    {name:'elmof' ,id:3}
+  ])
+  const [showEvents , setShowEvents] = useState(true)
+  const handleClick = (id) =>{
+    console.log('clicked on buttin')
+    setUnis(unis.filter((uni) => {
+      return id !== uni.id
+    }))
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {showEvents && unis.map((uni, index) => (
+        <div key={uni.id}>
+          <h2> shomare {index+1} - {uni.name}</h2>
+          <button onClick={() => handleClick(uni.id)}> delete {uni.name}</button>
+        </div>
+      ))}
     </div>
   );
 }
