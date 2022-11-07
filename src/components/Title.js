e60880f6e2497cdc96267b1754d9b72046d87b47
unknown <mohammadalisaeidi@yahoo.com>
2022-11-07 18:48:03 +0330

    Global && components stylesheets
    E39

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,9 +1,10 @@
+import './Title.css'
 export default function Title({onvan, zironvan}) {
     return(
-        <>
+        <div className='title-block'>
             <h1 className="title">{onvan}</h1>
             <br />
             <h2 className="subtitle">{zironvan}</h2>
-        </>
+        </div>
     )
 }
